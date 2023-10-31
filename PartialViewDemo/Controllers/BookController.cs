b0d925edd680526bec092e4b70b273289158272b
ChidanandGurav <chidugurav6@gmail.com>
2023-10-31 09:57:03 +0530

    Add project files.

---
+++ PartialViewDemo/Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewDemo.Data;
+using PartialViewDemo.Models;
+
+namespace PartialViewDemo.Controllers
+{
+    public class BookController : Controller
+    {
+        ApplicationDbContext context;
+
+        public BookController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model = context.Books.ToList();
+            return View(model);
+        }
+    }
+}
---
+++ PartialViewDemo/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PartialViewDemo.Models
+{
+    [Table("Book")]
+    public class Book
+    {
+        [Key]//pk in col in DB
+        public int Id { get; set; }
+       
+        public string? Name { get; set; }
+        
+        public string? Author { get; set; }
+        
+        public int Price { get; set; }
+    }
+}
